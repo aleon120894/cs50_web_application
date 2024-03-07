4909da3ad4da1beef978e10db311404a1def9cda
aleon120894 <user@user>
2024-03-07 12:56:07 +0200

    Trying-to-add-main-page-into-project

--- learning/learning/urls.py
+++ learning/learning/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+# import views
 
 
 urlpatterns = [
     # path('/', views.index, name="index"),
+
     path('admin/', admin.site.urls),
     path('search/', include('search.urls')),
     path('wiki/', include('wiki.urls'))
