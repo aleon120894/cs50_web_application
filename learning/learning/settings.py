3862b71f02b57cc5dc077ae2679169a230c815a2
aleon120894 <user@user>
2024-03-25 16:59:45 +0200

    Add-mail.User-migration-add-mail-application

--- learning/learning/settings.py
+++ learning/learning/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'search',
+    'mail',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'learning.urls'
+AUTH_USER_MODEL = 'mail.User'
 
 TEMPLATES = [
     {
