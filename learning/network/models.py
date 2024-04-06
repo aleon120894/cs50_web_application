19fcd4175a3781fae91596ff1923cd1b7b042653
aleon120894 <user@user>
2024-04-06 20:10:07 +0300

    Wirking-with-posts-page

--- learning/network/models.py
+++ learning/network/models.py
@@ -27,5 +27,8 @@ class User(AbstractUser):
     )
 
 
-class Post:
-    pass
+class Post(models.Model):
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
