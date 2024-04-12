eab730ce74e1c90d5815050e2d7429d87a487beb
aleon120894 <user@user>
2024-04-12 12:50:10 +0300

    Trying-to-resolve-assertion-error

--- learning/network/urls.py
+++ learning/network/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('add/', views.add_post, name='add_post'),
     path("login/", views.login_view, name="login"),
+
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
     path("account_page/", views.account_page, name="account_page"),
